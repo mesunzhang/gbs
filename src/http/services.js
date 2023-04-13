b154def3d3c0e9c1845c83d4119e73c91ee1ab14
zhangyutian <443083118@qq.com>
2023-04-13 15:04:17 +0800

    init

--- src/http/services.js
+++ src/http/services.js
@@ -5,31 +5,29 @@ import { GET } from './request'
  * @returns {Promise<any>}
  */
 export function commonStepListV3() {
-  return GET(
-    '/api/HomeController/commonStepListV3?changeType=0&_=1680314620694'
-  )
+  return GET('/api/commonStepListV3?changeType=0&_=1680314620694')
 }
 
 export function needReOauth() {
-  return GET('/api/HomeController/needReOauth')
+  return GET('/api/needReOauth')
 }
 
 export function getUserInfo() {
-  return GET('/api/HomeController/userInfoV2')
+  return GET('/api/userInfoV2')
 }
 
 export function randomStepDetailUnSpec() {
-  return GET('/api/HomeController/randomStepDetailUnSpec')
+  return GET('/api/randomStepDetailUnSpec')
 }
 
 export function getStepDetail() {
-  return GET('/api/HomeController/stepDetail')
+  return GET('/api/stepDetail')
 }
 
 export function createOrderBase() {
-  return GET('/api/GbsPayController/createOrderBase?type=common_pay_type_1')
+  return GET('/api/createOrderBase?type=common_pay_type_1')
 }
 
 export function insertMsg(params) {
-  return GET('api/HomeController/insertMsg', params)
+  return GET('api/insertMsg', params)
 }
